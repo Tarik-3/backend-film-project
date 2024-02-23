dee267a0577d65051ab8d81625c5e338f65779e4
Tarik-3 <119797713+Tarik-3@users.noreply.github.com>
2024-02-23 15:47:59 +0100

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,10 @@
+def test() {
+   echo"I am Tarik, and I am here"
+        sh "mvn package"
+}
+
+def running() {
+  echo"I am Tarik, and I am a hero"
+        echo"Running with ${CREDENTIAL}"
+        echo "${params.VERSION}"
+}
