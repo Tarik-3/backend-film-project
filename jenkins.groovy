ffea14661ffa76dfdf945b2fe09022f7f48cd9f0
Tarik-3 <119797713+Tarik-3@users.noreply.github.com>
2024-02-23 18:08:50 +0100

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,13 +5,21 @@ def test() {
 
 def building() {
    echo "I will build the project image"
-   sh "docker build -t tarik3/jenkins-test:${params.VERSION} ."
+   withCredentials([usernamePassword(credentialsId:"Tarik-docker",passwordVariable:"PASS", usernameVariable: "USER")]){
+      sh "docker build -t tarik3/jenkins-test:${params.VERSION} ."
+      sh "echo $PASS | docker login -u $USER -password--stdin" 
+      sh "docker push tarik3/jenkins-test:${params.VERSION} "
+   }
+   
    
 }
 def running() {
   echo"I will run the image"
    sh "docker run tarik3/jenkins-test:${params.VERSION} "
-        
-    
+}
+def deploy() {
+   echo "Deployement"
+   sh"docker login -u "
+   sh"docker push"
 }
 return this
