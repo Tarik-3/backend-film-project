f78581e97c3e3a0d7627ed9761c0ed41d8333d7b
Tarik-3 <119797713+Tarik-3@users.noreply.github.com>
2024-02-23 16:31:46 +0100

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,10 +3,15 @@ def test() {
         sh "mvn package"
 }
 
+def building() {
+   echo "I will build the project image"
+   sh "docker build -t tarik3/jenkins-test"
+   
+}
 def running() {
-  echo"I am Tarik, and I am a hero"
-        echo"Running with ${CREDENTIAL}"
-        echo "${params.VERSION}"
+  echo"I will run the image"
+   sh "docker run tarik3/jenkins-test:${params.VERSION} "
+        
     
 }
 return this
